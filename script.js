3161ce36b9556f5d1736b464d50caafe5316af2f
Igarashi12 <kigarashi@dl-techx-xedge.net>
2025-11-25 14:42:16 +0900

    カスタムURLに修正

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ createApp({
       };
 
       // 実際のAPI URLに置き換えてください
-      const response = await axios.post('https://m3h-igarashi-containerapp-vnet.redcoast-9c30ccab.japaneast.azurecontainerapps.io/api/INSERT', param);
+      const response = await axios.post('https://backapp-igarashi.m3harbor.net/api/INSERT', param);
       console.log(response.data);
     };
 
     const readData = async () => {
       // 実際のAPI URLに置き換えてください
-      const response = await axios.get('https://m3h-igarashi-containerapp-vnet.redcoast-9c30ccab.japaneast.azurecontainerapps.io/api/SELECT');
+      const response = await axios.get('https://backapp-igarashi.m3harbor.net/api/SELECT');
       console.log(response.data);
       dataList.value = response.data.List;
     };
